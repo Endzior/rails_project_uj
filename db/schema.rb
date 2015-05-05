08507607a6795a8686ecfe31099ca039b019cd39
Kacper Stąpor <endzior93@gmail.com>
2015-05-05 13:59:43 +0000

    added first and last name to sign up and edition of user profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505131030) do
+ActiveRecord::Schema.define(version: 20150505134359) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20150505131030) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
