8e4f524b72c11fa05ff93aef931513ea4bb452fb
Kacper Stąpor <endzior93@gmail.com>
2015-05-18 22:23:39 +0000

    added feature testing ticket proper showing

--- spec/features/projects_features_spec.rb
+++ spec/features/projects_features_spec.rb
@@ -25,10 +25,22 @@ feature 'projects' do
     it 'user sees projects name, it\'s description and the leader of the project' do
       visit project_path(@project_without_tickets)
       
-      expect(page).to have_content(@project_without_tickets.user.full_name)
       expect(page).to have_content(@project_without_tickets.name)
-      expect(page).to have_content(@project_without_tickets.description) 
-
+      expect(page).to have_content(@project_without_tickets.description)
+      expect(page).to have_content(@project_without_tickets.user.full_name)
+      expect(page).to have_no_content('very' || 'low' || 'normal' || 'high' || 'easy' || 'hard')
+    end
+  end
+  
+  describe 'user visits specific project page with many tickets assigned to it' do
+    it 'user sees projects name, it\'s description, leader of the project and many tickets' do
+      visit project_path(@project_with_100_tickets)
+      
+      @project_with_100_tickets.tickets.each do |ticket|
+        expect(page).to have_content(ticket.name)
+        expect(page).to have_content(ticket.get_priority)
+        expect(page).to have_content(ticket.get_difficulty)
+      end
     end
   end
 end
