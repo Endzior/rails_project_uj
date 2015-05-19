29d29bb8fd5882d7330f6b37660632e57e86a38c
Kacper Stąpor <endzior93@gmail.com>
2015-05-19 05:12:00 +0000

    finished a feature test for tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,31 +4,43 @@ class Ticket < ActiveRecord::Base
   
   def get_priority
     case priority
-    when 1
-      return 'very low'
-    when 2
-      return 'low'
-    when 3
-      return 'normal'
-    when 4
-      return 'high'
-    else
-      return 'very high'
+      when 1
+        return 'very low'
+      when 2
+        return 'low'
+      when 3
+        return 'normal'
+      when 4
+        return 'high'
+      else
+        return 'very high'
     end
   end
   
   def get_difficulty
     case difficulty
-    when 1
-      return 'very easy'
-    when 2
-      return 'easy'
-    when 3
-      return 'medium'
-    when 4
-      return 'hard'
-    else
-      return 'very hard'
+      when 1
+        return 'very easy'
+      when 2
+        return 'easy'
+      when 3
+        return 'medium'
+      when 4
+        return 'hard'
+      else
+        return 'very hard'
     end
   end
+  
+  def get_status
+    case status
+      when 1
+        return 'finished'
+      when -1
+        return 'cancelled'
+      else
+        return 'active'
+    end
+  end
+    
 end
--- spec/features/tickets_features_spec.rb
+++ spec/features/tickets_features_spec.rb
@@ -6,26 +6,38 @@ feature 'tickets' do
     @user = create(:user)
     @project = create(:project)
     @ticket = create(:ticket)
+    visit new_user_session_path
+      
+    fill_in 'user_email', :with => @user.email
+    fill_in 'user_password', :with => 'password'
+      
+    click_button 'Log in'
   end
   
   describe 'user adds a new ticket to existing project' do
     it 'user sees page with the new ticket' do
-      visit new_user_session_path
-      
-      fill_in 'user_email', :with => @user.email
-      fill_in 'user_password', :with => 'password'
-      
-      click_button 'Log in'
-      
       visit project_path(@project)
       
       click_link 'Add a ticket to this project'
+      
+      fill_in 'ticket_name', :with => @ticket.name
+      fill_in 'ticket_description', :with => @ticket.description
+      select @ticket.get_priority, :from => 'ticket_priority'
+      select @ticket.get_difficulty, :from => 'ticket_difficulty'
+      select @ticket.get_status, :from => 'ticket_status'
+      
+      click_button 'Create Ticket'
+      
+      expect(page.current_url).to eq 'http://www.example.com/projects/' + @project.id.to_s + '/tickets/' + @project.tickets.last.id.to_s
+      expect(page).to have_content(@ticket.name)
+      expect(page).to have_content(@ticket.description)
+      expect(page).to have_content(@project.name)
     end
   end
   
   describe 'user views specific ticket' do
     it 'user sees page with ticket name, description' do
-      
+      visit project_ticket_path(@ticket.project_id, @ticket)
       
     end
   end
